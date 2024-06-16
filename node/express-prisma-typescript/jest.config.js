6fed62dc3a759c8536baf787c10936dabe9e3219
Franco Palumbo <palumbofranco9a@gmail.com>
2024-06-16 18:14:20 -0300

    Fix tests

--- node/express-prisma-typescript/jest.config.js
+++ node/express-prisma-typescript/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'tsconfig.json'
-    }],
-  },
-};
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ['./src/tests/config.ts'],
+  moduleNameMapper:{
+    '^@utils': '<rootDir>/src/utils',
+    '^@domains/(.*)': '<rootDir>/src/domains/$1',
+    '^@s3-bucket': '<rootDir>/src/s3-bucket',
+  }
+}
---
+++ node/express-prisma-typescript/src/tests/mocks.ts
@@ -0,0 +1,7 @@
+export const UserRepository = {
+  getById: jest.fn(),
+  getRecommendedUsersPaginated: jest.fn(),
+  getByUsernamePrefix: jest.fn(),
+  updateProfilePicture: jest.fn(),
+  delete: jest.fn(),
+};
